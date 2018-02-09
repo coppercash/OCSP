fb85f80c0c1afe389e38242c9cd77685a30e71b8
William <coderdreamer@gmail.com>
2018-02-09 09:42:38 +0800

    Initial Commit

---
+++ OCSP/OCSP.h
@@ -0,0 +1,13 @@
+//
+//  OCSP.h
+//  OCSP
+//
+//  Created by William on 09/02/2018.
+//  Copyright © 2018 coppercash. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCSP : NSObject
+
+@end
---
+++ OCSP/OCSP.m
@@ -0,0 +1,13 @@
+//
+//  OCSP.m
+//  OCSP
+//
+//  Created by William on 09/02/2018.
+//  Copyright © 2018 coppercash. All rights reserved.
+//
+
+#import "OCSP.h"
+
+@implementation OCSP
+
+@end
