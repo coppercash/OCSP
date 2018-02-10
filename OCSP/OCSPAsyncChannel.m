36d60daa4841a0267583426a29e73021bf65697b
William <coderdreamer@gmail.com>
2018-02-10 13:14:02 +0800

    Async

--- OCSP/OCSPAsyncChannel.m
+++ OCSP/OCSPAsyncChannel.m
@@ -9,20 +9,70 @@
 #import "OCSPAsyncChannel.h"
 
 @implementation OCSPAsyncChannel
-- (void)receiveIn:(void(^)(id, BOOL))callback { if (callback) { callback(nil, NO); } }
-@end
 
-@implementation OCSPAsyncReadWriteChannel
-
-- (void)receiveIn:(void(^)(id data, BOOL ok))callback
+- (void)receiveWithOn:(dispatch_queue_t)queue
+             callback:(void(^)(id, BOOL))callback
 {
-    
+    if (callback) { callback(nil, NO); }
 }
 
-- (void)send:(id)value
-        with:(void(^)(BOOL ok))callback
+@end
+
+@implementation OCSPAsyncReadWriteChannel {
+    dispatch_queue_t
+    _writerWatch,
+    _readerWatch;
+}
+
+- (instancetype)init
 {
-    
+    if (!(
+          self = [super init]
+          )) { return nil; }
+    _writerWatch = dispatch_queue_create
+    (
+     "ocsp.ayncchannel.watch.writer",
+     DISPATCH_QUEUE_SERIAL
+     );
+    _readerWatch = dispatch_queue_create
+    (
+     "ocsp.ayncchannel.watch.reader",
+     DISPATCH_QUEUE_SERIAL
+    );
+    return self;
+}
+
+- (void)receiveWithOn:(dispatch_queue_t)queue
+             callback:(void(^)(id, BOOL))callback;
+{
+    dispatch_async(_readerWatch, ^{
+        id
+        data;
+        BOOL
+        ok = [self receive:&data];
+        if (!(
+              callback
+              )) { return; }
+        dispatch_async(queue, ^{
+            callback(data, ok);
+        });
+    });
+}
+
+- (void)send:(id)data
+      withOn:(dispatch_queue_t)queue
+    callback:(void(^)(BOOL ok))callback
+{
+    dispatch_async(_writerWatch, ^{
+        BOOL
+        ok = [self send:data];
+        if (!(
+              callback
+              )) { return; }
+        dispatch_async(queue, ^{
+            callback(ok);
+        });
+    });
 }
 
 @end
